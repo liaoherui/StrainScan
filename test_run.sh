f30a83e0ae40c77f914feda102fdb229bb88ff64
Ray <851836818@qq.com>
2021-12-24 17:00:08 +0800

    Update test_run.sh

--- test_run.sh
+++ test_run.sh
@@ -1,5 +1,5 @@
 python StrainScan_build.py -i Test_genomes -o DB_Small
 
-python StrainScan.py -i Sim_Data/GCF_003812785_10X.fq -d DB_Small -o Test_Sim/GCF_003812785
+python StrainScan.py -i Sim_Data/GCF_003812785.fq -d DB_Small -o Test_Sim/GCF_003812785
 
 python StrainScan.py -i Sim_Data_mul/GCA_000144385_5X_GCF_008868325_5X.fq -d  DB_Small -o Test_Sim/GCA_000144385_5X_GCF_008868325_5X 
