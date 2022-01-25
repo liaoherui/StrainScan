90925cfceb5f864ab2b68079039219c17b1f3e22
Ray <851836818@qq.com>
2022-01-25 20:36:50 +0800

    Update pre_db.sh

--- Download_DB_script/pre_db.sh
+++ Download_DB_script/pre_db.sh
@@ -1,4 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yclHCQYpatDChOO2lVjQorQ43K4H1ltz' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1yclHCQYpatDChOO2lVjQorQ43K4H1ltz" -O DB_Pre.zip && rm -rf /tmp/cookies.txt  &&\
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qhc17ZSRop0hp5lrM2sAQiffv0PXuQ6r' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qhc17ZSRop0hp5lrM2sAQiffv0PXuQ6r" -O DB_Pre.zip && rm -rf /tmp/cookies.txt  &&\
 
 
 unzip DB_Pre.zip &&\
