4bddc3503bcf0d882f930de1182a7d9fc93a6585
Ray <851836818@qq.com>
2022-05-26 13:43:19 +0800

    Update mtb_db.sh

--- Download_DB_script/mtb_db.sh
+++ Download_DB_script/mtb_db.sh
@@ -1,4 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1jjpDVVN7yD4f7r8yp_30cIgfTJ5KoGUt' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1jjpDVVN7yD4f7r8yp_30cIgfTJ5KoGUt" -O DB_Mtb.zip && rm -rf /tmp/cookies.txt  &&\
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=18pPGyHODRwYV_d-l-xOKiLkbj7WcHCR_' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=18pPGyHODRwYV_d-l-xOKiLkbj7WcHCR_" -O DB_Mtb.zip && rm -rf /tmp/cookies.txt  &&\
 
 
 unzip DB_Mtb.zip &&\
