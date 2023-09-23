fd287155f98441d211a1953f686c07c5652ba3dd
Ray <851836818@qq.com>
2023-09-23 12:12:50 +0800

    Update sau_db.sh

--- Download_DB_script/sau_db.sh
+++ Download_DB_script/sau_db.sh
@@ -1,1 +1,8 @@
-https://drive.google.com/file/d/105qmCZFgVh_2i-nQuB80emyVVqcSQBhi/view?usp=sharing
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=105qmCZFgVh_2i-nQuB80emyVVqcSQBhi' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=105qmCZFgVh_2i-nQuB80emyVVqcSQBhi" -O DB_Sau.zip && rm -rf /tmp/cookies.txt  &&\
+
+
+unzip DB_Sau.zip &&\
+rm  DB_Sau.zip
+
+
+
